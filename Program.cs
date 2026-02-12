c8f84e52eca8c5a3323e1073b600796c1a149b99
Yatripatel17 <yatripatel1710@gmail.com>
2026-02-12 17:58:01 -0500

    Git initialized

---
+++ Program.cs
@@ -0,0 +1,35 @@
+namespace Week5Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddAuthorization();
+        builder.Services.AddControllers();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        app.UseSwagger();
+        app.UseSwaggerUI(c => 
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Week5Api V1");
+            c.RoutePrefix = "swagger"; 
+        });
+        
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        app.MapGet("/hello", () => "Hello from your Azure API running .NET 9");
+
+        app.Run();
+    }
+}
